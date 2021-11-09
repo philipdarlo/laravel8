3e739671d25f9ee98904256ca41b5be474992341
Philip Darlington <philip@wubbleyou.co.uk>
2021-11-09 14:07:15 +0000

    finish ep 34

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest('published_at')->get()
+        'posts' => Post::latest('published_at')->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -33,12 +34,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
